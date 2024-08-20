4999f5455049875ce67cd131e2d7d2a141ecfa60
nanda <nanda4480@gmail.com>
2024-08-20 12:40:44 +0200

    add remove option for todo webapp

--- list.py
+++ list.py
@@ -6,7 +6,7 @@ print(1 in checked_list)
 print(8 not in checked_list)
 print(5 not in checked_list)
 '''
-
+'''
 #index
 index_exp = [["wood"], [1,2,3], [11,22,33], ["thank"]]
 print(index_exp[0])
@@ -33,4 +33,55 @@ example = ["thank", "good", "ok"]
 print(example)
 example[0] = "very"
 print(example)
+'''
+'''
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list1[-1::-2])
+list1[0:3] = [11,22,33]
+print(list1)
+
+l1 = [1,2,3]
+l2 = [4,5,6]
+l4 = ['a','b','c']
+l3 = l1+l2+l4
+print(l3)
+l3.extend(['x', 'y', 'z'])
+print(l3)
+l1 = l1 * 3
+print(l1)
+
+#iterating a list
+for item in range(len(l3)):
+    print(l3[item])
+
+for item in range(len(l3)-1,-1,-1):
+    print(l3[item])
+
+i=0
+while i < len(l3):
+    print(l3[i])
+    i+=1
+'''
+#list methods
+list1 = [1, 2, 3, 4, 5]
+list1.append('a')
+print(list1)
+list1.extend(['b','c'])
+print(list1)
+list1.extend(['xyz'])
+print(list1)
+list1.insert(8, 'ddd')
+print(list1)
+list5= list1.copy()
+print('new', list5)
+list5.pop() #delete the last list item
+print(list5)
+
+list5.pop(0) #with idex
+print(list5)
+
+list5.remove('ddd') # remove the item directly
+print(list5)
+
+
 
--- string.py
+++ string.py
@@ -54,11 +54,102 @@ display_no = card_no[15::]
 star = '*' * 4 + ' '
 print(star + star + star + display_no)
 '''
+
+'''
 #find user id and domain name from the email address
 email = input("enter your favorite email: ")
 atrate = email.find('@') #this find method will show the index of @
 print('user id:', email[0:atrate])
 print('domain id:', email[atrate+1:])
+'''
+'''
+#check a strig is palindrome
+string1 = input("enter your favorite string ")
+rev = string1[::-1]
+print(rev)
+if string1 == rev:
+    print('string is palindrome')
+else:
+    print('string is not palindrome')
 
+'''
+'''
+#convert the string into palindrome
+string1 = input("enter your favorite string ")
+rev = string1[::-1]
+print(rev)
+print(string1+rev)
+'''
+'''
+#find day month and year from a date
+date = input("Enter the date fowwing format dd/mm/yyyy")
+date_split=date.split('/')
+print(date_split)
+day = date_split[0]
+month = date_split[1]
+year = date_split[2]
+print('day: ',day,'month:', month,'year:', year)
+'''
+'''
+#check two strings are anagram
 
+s1 = input("enter string1 ")
+s2 = input("enter string2 ")
 
+if len(s1) != len(s2):
+    print('not anagram')
+else:
+
+    for letter in s1:
+        if letter not in s2:
+            print('not anagram')
+            break
+    else:
+        print('anagram')
+'''
+'''
+#Rearrange : lowercase then uppercase
+string = 'dsfdAAdfsTHKGF'
+lower = ''
+upper = ''
+for letters in string:
+    if letters.islower():
+        lower += letters
+    else:
+        upper += letters
+
+print(lower+upper)
+'''
+'''
+#remove punctuations in a string
+punctuation = "!@#$%^&*()[]{}:;',./?|"
+string = input("enter string with punctuation ")
+word = ''
+sp_letters = ''
+for letter in string:
+    if letter in punctuation:
+        sp_letters += letter
+    else:
+        word += letter
+print(word, '----', sp_letters)
+'''
+
+'''
+#print functions
+a=10
+b='nanda'
+c='learn'
+print(a,b,c,sep='-')
+print(a)
+print(b)
+print(c)
+print(a, end='#')
+print(b, end='#')
+print(c, end='\n')
+#formated printing
+x=22
+y=7
+z=x/y
+print('division of {0}, and {1} is{2}'.format(x,y,z))
+print(f"division of {x} and {y} is{z}")
+'''
